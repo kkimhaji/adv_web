4167d670f7dc88531db824e619d589fa6793d0d3
하지현 <kkim.haji@gmail.com>
2022-03-10 17:54:52 +0900

    2nd_last

--- 02/app.js
+++ 02/app.js
@@ -43,3 +43,34 @@ var me = {
 for(var i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
+
+var todos = ['운동'];
+
+var todo = '게임';
+todos.push(todo);
+
+// console.log(todos);
+
+todos.forEach(function(todo){
+    console.log(todo);
+});
+
+//update
+var updateTodo = '게임';
+var updateIndex = todos.findIndex(function(todo){
+    return todo == updateTodo;
+});
+
+console.log(updateIndex);
+todos[updateIndex] = '공부';
+console.log(todos);
+
+
+//delete
+
+var deleteTodo = '공부';
+var newTodos = todos.filter(function(todo){
+    return todo != deleteTodo;
+});
+
+console.log(newTodos);
